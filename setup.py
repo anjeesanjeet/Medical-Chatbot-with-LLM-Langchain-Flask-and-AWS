0f5037e06e3cba88b504f201dc17a21a23eb979d
Sanjeet Kumar <anjeesanjeet@gmail.com>
2025-09-26 11:21:23 +0530

    Reuirements Installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sanjeet Kumar",
+    author_email="anjeesanjeet@gmail.com",
+    packages=find_packages(),
+    install_requires=[],  # Dependencies are now listed in requirements.txt
+)
